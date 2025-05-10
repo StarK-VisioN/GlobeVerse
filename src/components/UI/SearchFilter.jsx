80697340dec09e8b0fdfa952352ed8426bd87326
= <=>
2025-05-10 17:31:22 +0530

    Add layout component

--- src/components/UI/SearchFilter.jsx
+++ src/components/UI/SearchFilter.jsx
@@ -26,7 +26,7 @@ function SearchFilter({search, setSearch, filter, setFilter, countries, setCount
                 placeholder="search"
                 value={search}
                 onChange={handleInputChange}
-                autoFocus
+                className="border border-gray-300 px-2 py-1 focus:outline-none focus:border-blue-500"
             />
         </div>
 
